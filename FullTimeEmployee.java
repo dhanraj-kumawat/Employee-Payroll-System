83d82e482b0977242945d75a6d403441595ee891
dhanraj-kumawat <kumawatdhanraj8@gmail.com>
2023-10-14 10:15:05 +0530

    added FullTimeEmployee class

---
+++ FullTimeEmployee.java
@@ -0,0 +1,15 @@
+ class FullTimeEmployee extends Employee{
+    private double monthlySalary ;
+
+    // creating constructor
+    public FullTimeEmployee(String name, int id, double monthlySalary) {
+        super(name, id);
+        this.monthlySalary = monthlySalary;
+    }
+
+    @Override
+    public double calculateSalary() {
+        return monthlySalary;
+    }
+    
+}
